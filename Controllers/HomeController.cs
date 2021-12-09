3cac6197d3f582458dec470be3dd008a52a099f0
Gritsak Alexandra <74914400+gritsak-alexandra@users.noreply.github.com>
2021-12-09 20:09:20 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using shhop.Models;
+using shhop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shhop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ApplicationContext context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public IActionResult Index(string category)
+        {
+            string _category = category;
+            string _categoryItem = null;
+            IEnumerable<Thing> things = null;
+            string currCategory = "";
+            string currCategoryItem = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                things = context.Thing.OrderBy(t => t.id);
+
+            }
+            else
+            {
+                things = context.Thing.Where(t => t.categoryId == int.Parse(category)).OrderBy(t => t.id);
+            }
+
+            var thingObj = new ThingListViewModel
+            {
+                allThings = things,
+                currCategory = currCategory,
+                currCategoryItem = currCategoryItem
+            };
+            ViewBag.Title = "Page of things";
+            return View(thingObj);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using shhop.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace shhop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationContext _applicationContext;
+        private readonly UserManager<User> _userManager;
+
+        public ShopController(ApplicationContext applicationContext, UserManager<User> userManager)
+        {
+            _applicationContext = applicationContext;
+            _userManager = userManager;
+        }
+
+        public IActionResult GetCatHud()
+        {
+            return RedirectToAction("Index", "Home", new { category = "4" });
+        }
+        public IActionResult GetCatMang()
+        {
+            return RedirectToAction("Index", "Home", new { category = "2" });
+        }
+
+        public async Task<IActionResult> AddToCart(int prodId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            await _applicationContext.Cart.AddAsync(new Cart() { UserId = user.Id, ThingId = prodId });
+            await _applicationContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> ShowCart()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var tempcart = _applicationContext.Cart
+                .Where(c => c.UserId == user.Id)
+                .Select(c => c.Thing)
+                .ToList();
+            var cart = new Dictionary<Thing, int>();
+            foreach(var thing in tempcart)
+            {
+                int count = tempcart.Count(t => t == thing);
+                cart.TryAdd(thing, count);
+            }
+            return View(cart);
+        }
+        public async Task<IActionResult> Confirm()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var cart = _applicationContext.Cart
+                .Where(c => c.UserId == user.Id);
+
+            _applicationContext.RemoveRange(cart);
+            await _applicationContext.SaveChangesAsync()
+;
+            return View();
+        }
+    }
+}
