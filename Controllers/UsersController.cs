e8a87f5a0cb90931aedde3cea9372fce628c78cf
Gritsak Alexandra <74914400+gritsak-alexandra@users.noreply.github.com>
2021-12-10 13:39:04 +0300

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -23,15 +23,15 @@ namespace shhop.Controllers
     public IActionResult Create() => View();
 
     [HttpPost]
-    public async Task<IActionResult> Create(CreateUserViewModel model)
+    public async Task<IActionResult> Create(CreateUserViewModel model) //создание пользователя
     {
         if (ModelState.IsValid)
         {
-            User user = new User { Email = model.Email, UserName = model.Email, Year = model.Year };
-            var result = await _userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
+            User user = new User { Email = model.Email, UserName = model.Email, Year = model.Year }; //получаем модель и создаем пользователя
+            var result = await _userManager.CreateAsync(user, model.Password); //получаем результат создания
+            if (result.Succeeded) //если успешно
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); //возвращаем результат
             }
             else
             {
@@ -44,33 +44,33 @@ namespace shhop.Controllers
         return View(model);
     }
 
-    public async Task<IActionResult> Edit(string id)
+    public async Task<IActionResult> Edit(string id) //вывод инфы о пользователе
     {
-        User user = await _userManager.FindByIdAsync(id);
-        if (user == null)
+        User user = await _userManager.FindByIdAsync(id); //получаем пользователя
+        if (user == null) //если не сущетсвует
         {
             return NotFound();
         }
-        EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.Year };
+        EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, Year = user.Year }; //вывод информации
         return View(model);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Edit(EditUserViewModel model)
+    public async Task<IActionResult> Edit(EditUserViewModel model) //изменение информации о пользователе
     {
-        if (ModelState.IsValid)
+        if (ModelState.IsValid) 
         {
-            User user = await _userManager.FindByIdAsync(model.Id);
-            if (user != null)
+            User user = await _userManager.FindByIdAsync(model.Id); //получаем модель пользователя по id
+            if (user != null) //если пользователь существует
             {
-                user.Email = model.Email;
+                user.Email = model.Email; //устанавливаем значения
                 user.UserName = model.Email;
                 user.Year = model.Year;
 
-                var result = await _userManager.UpdateAsync(user);
-                if (result.Succeeded)
+                var result = await _userManager.UpdateAsync(user); //получаем результат изменнеий 
+                if (result.Succeeded) //если успешно
                 {
-                    return RedirectToAction("Index");
+                    return RedirectToAction("Index"); //выводим  результат
                 }
                 else
                 {
@@ -85,12 +85,12 @@ namespace shhop.Controllers
     }
 
     [HttpPost]
-    public async Task<ActionResult> Delete(string id)
+    public async Task<ActionResult> Delete(string id) //удаление пользователя
     {
-        User user = await _userManager.FindByIdAsync(id);
-        if (user != null)
+        User user = await _userManager.FindByIdAsync(id); //получаем id пользователя
+        if (user != null) //если существует
         {
-            IdentityResult result = await _userManager.DeleteAsync(user);
+            IdentityResult result = await _userManager.DeleteAsync(user); //удаление пользователя
         }
         return RedirectToAction("Index");
     }
